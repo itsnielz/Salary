cf6786d2a42d6d2aa69b12794a397bcf89dfb98c
itsnielz <96enjoy.v2@gmail.com>
2022-05-23 17:39:48 +0700

    css

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a href="#"><img src="{{asset('images/logo/logo salary.png')}}" alt="image/x-icon"></a>
+     
+        
+      
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </div>
+      </div>
+    </div>
+  </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,14 +2,16 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-  <link rel="icon" href="{{ asset('image/logo/logo salary.png') }}" type="image/x-icon">
+  <link rel="icon" href="{{ asset('images/logo/logo salary.png') }}" type="image/x-icon">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ config('app.name') }}</title>
 
- 
+  <link rel="stylesheet" href="{{ asset('bootstrap/dist/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/fontawesome/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/kanit_thai/index.css') }}">
+  <link rel="stylesheet" href="{{asset('css/main.css')}}">
+
   @yield('style')
 </head>
 
@@ -17,7 +19,8 @@
 
   @yield('content')
 
- 
+  <script src="{{ asset('bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('js/fontawesome/all.min.js') }}"></script>
 
 
   @yield('script')
